8620d60564cca2a9d5cb3ce4379754705569fb84
a-yan <mameiyuanguo@gmail.com>
2022-06-27 10:52:22 +0900

    抜粋文、本文の記載がない場合のエラーを修正

--- article-list.php
+++ article-list.php
@@ -16,21 +16,10 @@
           <div class="p-productItem__body">
             <p class="p-productItem__name"><?php the_title(); ?></p>
             <?php
-              if (has_excerpt()) {
-                the_excerpt();
+              if ($post->post_content == "") {
+                echo the_excerpt();
               } else {
-                global $post;
-                preg_match_all('/<h[2]>.+<\/h[2]>/u', $post->post_content, $matches_heading);
-                preg_match_all('/<p>.+<\/p>/u', $post->post_content, $matches_text);
-                $heading = $matches_heading[0][0];
-                $heading = strip_tags($heading);
-                $heading = '<p class="p-productItem__ttl">'.$heading.'</p>';
-                echo $heading;
-
-                $text = $matches_text[0][0];
-                $text = strip_tags($text);
-                $text = '<p class="p-productItem__txt">'.$text.'</p>';
-                echo $text;
+                echo the_content();
               }
             ?>
             <a href="<?php the_permalink(); ?>" class="p-productItem__btn">詳しく見る</a>
